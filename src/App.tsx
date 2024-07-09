db349f4cd547463dbb00b97b5196727f1e8d7c14
Andrea Frigido <frisoft@users.noreply.github.com>
2024-07-09 17:56:38 +0100

    Add app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SkullKingScoreKeeper from './components/skull-king-scorekeeper';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <SkullKingScoreKeeper />
+    </div>
+  );
+};
+
+export default App;
